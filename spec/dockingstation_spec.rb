773b4f610e7291be05fa8b58e4774efcbb451df9
Flora Davies <daviesfgo@gmail.com>
2021-03-03 16:06:39 +0000

    Creating working? and release_bike methods and writing corresponding Rspec tests

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,32 @@
 require 'dockingstation'
 
-describe DockingStation do
-  it {  expect( DockingStation.new()).to respond_to :release_bike }
+
+# describe DockingStation do
+#	it 'is of class DockingStation' do
+#		expect(subject).to be_instance_of DockingStation
+#	end
+# end
+
+describe Bike do
+alias_method :bike, :subject
+
+	it 'is of class Bike' do
+	  expect(bike).to be_instance_of Bike
+	end
+	it 'working?' do
+  	  expect(bike).to be_working
+    end
 end
+
+
+describe DockingStation do
+  it {  expect(DockingStation.new).to respond_to :release_bike }
+end
+
+describe DockingStation do
+  it 'releases a bike' do 
+    expect(DockingStation.new.release_bike).to be_instance_of Bike
+  end 
+end 
+
+
